1c2a14c4895057209035ac93c39fe71f8daef8bb
atlnery <49240604+atlnery@users.noreply.github.com>
2019-08-28 16:20:37 -0300

    Add files via upload

---
+++ ListaVetores(28-08)/ListaVetores - 04.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* intercala_a(int* a, int b, int* c, int d);
+
+int main(){
+
+  int v1[3] = {2,4,6};
+  int v2[4] = {3,5,7,9};
+  int v3[7];
+  int *v4;
+  v4 = intercala_a(v1, 3, v2, 4);
+  /*intercala_b(v1, 3, v2, 4, v3);*/
+
+}
+
+int* intercala_a(int* a, int tam_a, int* b, int tam_b){
+  int *vetor = (int*)malloc(4);
+  if (tam_b > tam_a){
+
+
+  }
+  else{
+
+  }
+
+}
