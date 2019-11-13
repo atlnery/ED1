800fc03d4f48eaa4a1b95bb286980c05e4988f0c
atlnery <49240604+atlnery@users.noreply.github.com>
2019-11-12 22:07:40 -0300

    Algumas alterações

--- OBI.c
+++ OBI.c
@@ -14,37 +14,45 @@ char oposto(char simbol){
 }
 
 char verifica_cadeia(char* c){
-  int i = 0;
-  while(c[i] != '\0'){
-    i++;
-  }
 
-  if(i%2 != 0) return 'N';
+  int cont = 0;
+  while (c[cont] != '\0'){
+    cont++;
+  } //saber qtd de elementos para serem comparados;
 
-  int k = i-1;
-  int j = 0;
+  if ( (cont%2) != 0) return 'N';
 
-  //printf("%d, %d\n",k,j); valor de k e j
+  char abre[50001];
+  char fecha[50001];
 
-  if ((k - j) == 1){
-    if (c[k] == (oposto(c[j])) ) {
-      return 'S';
+  int j = 0, k = 0;
+  int i;
+
+  for(i = 0; i < cont; i++){ //cria dois vetores com os que abrem e os que fecham
+    if(c[i] == '{' || c[i] == '[' || c[i] == '('){
+      abre[j] = c[i];
+      j++;
     }
     else{
-      return 'N';
-    }
-
-  }
-
-  while (j < (i/2)){
-    if ( c[k] == oposto(c[j]) ){ //O ÚLTIMO É O OPOSTO DO PRIMEIRO?
-      k--; j++;
-    }
-    else{
-      return 'N';
+      fecha[k] = c[i];
+      k++;
     }
   }
-  return 'S';
+
+  if (k != j) return 'N'; //se os vetores abre e fecha forem diferentes, tem caracter sem par.
+
+  int alpha = 0;
+  for(i = 0; i < k; i++){
+    j--;
+    if (fecha[i] != oposto(abre[j])){
+      alpha++;
+    }
+  }
+  if (alpha == 0) return 'S';
+  printf("%d\n",alpha);
+  printf("%c\n",fecha[alpha]);
+
+  return 'T';
 
 }
 
